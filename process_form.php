3029c114b6408bc9d214560b99a41913eda6ec68
Bofa115 <137942942+Bofa115@users.noreply.github.com>
2023-11-13 12:58:38 -0700

    Create process_form.php

---
+++ process_form.php
@@ -0,0 +1,65 @@
+<?php
+// Database connection details
+$host = 'ls-ccb1e52d3f78f1e75117f2a39182d15aac5f96d0.c4bmbwqvtvvv.ca-central-1.rds.amazonaws.com';
+$port = 3306; // The port number
+$username = 'dbmasteruser';
+$password = 'fgf(>.lC;+#XZBg$I[VGf:J03mK#61N$'; // Replace with your actual database password
+$database = 'Registration'; // Replace with your actual database name
+
+// Create a database connection
+$mysqli = new mysqli($host, $username, $password, $database, $port);
+
+// Check for connection errors
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Sanitize form data
+    $firstname = sanitize_input($_POST['firstname']);
+    $lastname = sanitize_input($_POST['lastname']);
+    $email = sanitize_input($_POST['email']);
+    $password = sanitize_input($_POST['password']);
+    $confirm_password = sanitize_input($_POST['confirm-password']);
+
+    // Validate and insert data into the database
+    if ($password === $confirm_password) {
+        // Hash the password before storing it
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // SQL query to insert data into the database
+        $query = "INSERT INTO users (firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
+        
+        // Prepare the statement
+        $statement = $mysqli->prepare($query);
+
+        // Bind parameters
+        $statement->bind_param('ssss', $firstname, $lastname, $email, $hashed_password);
+
+        // Execute the statement
+        $result = $statement->execute();
+
+        if ($result) {
+            echo 'Registration successful!';
+        } else {
+            echo 'Error: ' . $mysqli->error;
+        }
+
+        // Close the statement
+        $statement->close();
+    } else {
+        echo 'Password and Confirm Password do not match.';
+    }
+}
+
+// Close the database connection
+$mysqli->close();
+
+// Function to sanitize input data
+function sanitize_input($data)
+{
+    // Trim whitespaces and remove HTML and PHP tags
+    return htmlspecialchars(trim($data));
+}
+?>
